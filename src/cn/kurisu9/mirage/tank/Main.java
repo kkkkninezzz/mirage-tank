67571ed76ef6badba574a084d72f155be9d081a9
18581489108 <18581489108@163.com>
2022-02-15 17:21:25 +0800

    feat: 工具入口

---
+++ src/cn/kurisu9/mirage/tank/Main.java
@@ -0,0 +1,12 @@
+package cn.kurisu9.mirage.tank;
+
+/**
+ * @author kurisu9
+ * @description 入口
+ * @date 2022/2/15 17:20
+ **/
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
